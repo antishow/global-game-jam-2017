fd9829b7c5ab32156dc17c6621ff027ac5db3f38
Matthew Chase <antishow@gmail.com>
2017-01-21 17:44:27 -0500

    Added Vignette/Chroma shader to Main Camera, stub functions for IrisIn/IrisOut

--- Assets/CameraEffectsController.cs
+++ Assets/CameraEffectsController.cs
@@ -6,6 +6,7 @@ using UnityStandardAssets.ImageEffects;
 public class CameraEffectsController : MonoBehaviour {
     public static NoiseAndGrain NoiseEffect;
     public static ColorCorrectionCurves Saturation;
+    public static VignetteAndChromaticAberration Vignette;
     private static CameraEffectsController _instance;
 
     public float NoiseMin;
@@ -13,14 +14,33 @@ public class CameraEffectsController : MonoBehaviour {
     public float SaturationMin;
     public float SaturationMax;
 
+    public float VignetteMin = 0.1f;
+    public float VignetteMax = 0.25f;
+
+    public float ChromaMin = 0.15f;
+    public float ChromaMax = 0.4f;
+
     void Awake(){
         _instance = this;
         NoiseEffect = GetComponent<NoiseAndGrain>();
         Saturation = GetComponent<ColorCorrectionCurves>();
+        Vignette = GetComponent<VignetteAndChromaticAberration>();
     }
 
     public static void ApplyConfidenceToEffects(float Confidence){
         NoiseEffect.intensityMultiplier = Mathf.Lerp(_instance.NoiseMin, _instance.NoiseMax, Confidence);
         Saturation.saturation = Mathf.Lerp(_instance.SaturationMin, _instance.SaturationMax, Confidence);
+        Vignette.chromaticAberration = Mathf.Lerp(_instance.ChromaMin, _instance.ChromaMax, 1.0f - Confidence);
+        Vignette.intensity = Mathf.Lerp(_instance.VignetteMin, _instance.VignetteMax, 1.0f - Confidence);
+
+        Shader vignette = Vignette.vignetteShader;
+    }
+
+    public static void IrisIn(){
+        print("Once upon a time, in a procedurally generated city....");
+    }
+
+    public static void IrisOut(){
+        print("That's all, folks!");
     }
 }
