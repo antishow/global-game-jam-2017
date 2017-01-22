df36130199daa2be31c52f912d381915cc313572
Matthew Chase <antishow@gmail.com>
2017-01-22 00:56:21 -0500

    Quick title screen

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour {
+	public void NewGame(){
+		SceneManager.LoadScene("StreetScene");
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+}
